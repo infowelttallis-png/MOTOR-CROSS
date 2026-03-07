ba4c9833f85dbe837397d61089e7b7085a73e9ed
WELT TALLIS  INC <infowelttallis@gmail.com>
2026-03-07 21:27:50 +0300

    Delete client directory

--- client/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-// We remove StrictMode for maximum performance in the final render
-root.render(<App />);
